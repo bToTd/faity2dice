bf5398ef8a6994dea0713c99583dc40b1cbcda73
huangwenchuan <wenchuan.huang@rjoygame.com>
2020-04-20 16:44:43 +0800

    add xml2js

---
+++ src/fairyGUIparse/parsePkg.js
@@ -0,0 +1,15 @@
+/*
+ *@description: 解析 package.xml 
+ *@author: hwc 
+ *@date: 2020-04-20 16:27:56
+ *@version 1.0.0
+ */
+const parsePkg = (function(){
+    let parsePkg = function () {
+
+    }
+
+    return {
+        parsePkg: parsePkg
+    }
+})();
